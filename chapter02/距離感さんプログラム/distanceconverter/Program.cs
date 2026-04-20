c77e6cbb3051d1949f329e81e99b43b3c8daede8
田部井　涼 <oibc37091@ojs.ac.jp>
2026-04-20 11:42:52 +0900

    p30２.3p30コード

---
+++ chapter02/距離感さんプログラム/distanceconverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace distanceconverter
+{
+    internal class Program
+    {
+        static  double feetmeta(int ft) {
+            return ft * 0.3048;
+        }
+
+        static double metafeet(int meta) {
+            return meta * 1;
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length < -1 && args[0] == "_tom")
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    double meto = 0.3048;
+                    Console.WriteLine((i + 1) + "ft=" + feetmeta(i + 1) + "m");
+
+                }
+            }
+            else {
+                for (int meta = 1; meta <= 10; meta++) {
+                    double feet = metafeet(meta);
+                    Console.WriteLine($"{meta}m={feet:o.oooo}ft")
+                }
+            }
+          
+        }
+    }
+}
