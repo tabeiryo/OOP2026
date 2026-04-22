b0ecf5e76b2271a0bc0c4af20be52c3e377658e1
田部井　涼 <oibc37091@ojs.ac.jp>
2026-04-22 11:29:46 +0900

    tryparse

--- chapter02/距離感さんプログラム/distanceconverter/Program.cs
+++ chapter02/距離感さんプログラム/distanceconverter/Program.cs
@@ -18,29 +18,55 @@ static void Main(string[] args)
             string? arg = Console.ReadLine();
             args[0] = arg;
 
-            Console.WriteLine("表示範囲1を決めてください");
-            string? inputnum = Console.ReadLine();
-            int x = int.Parse(inputnum);
-
-            Console.WriteLine("表示範囲2を決めてください");
-            string? inputnm = Console.ReadLine();
-            int y = int.Parse(inputnm);
-
+             
             if (args[0] == "_tom")
             {
-                printfeetmetalist(x, y);
-                //for (int i = 0; i < 10; i++)
-                //{
-                //  double meto = 0.3048;
-                //Console.WriteLine($"{i}ft={meto:o.oooo}m"}
-             }
+                Console.WriteLine("表示範囲1(スタート)を決めてください");
+                string? inputnum = Console.ReadLine();
+
+                if (int.TryParse(inputnum, out var x))
+                {
+                     x = int.Parse(inputnum);
+
+                    Console.WriteLine("表示範囲2(ゴール)を決めてください");
+                    string? inputnm = Console.ReadLine();
+                    if (int.TryParse(inputnm, out var y))
+                    {
+                         y = int.Parse(inputnm);
+
+                        printfeetmetalist(x, y);
+                        //for (int i = 0; i < 10; i++)
+                        //{
+                        //  double meto = 0.3048;
+                        //Console.WriteLine($"{i}ft={meto:o.oooo}m"}
+                    }
+                    else Console.WriteLine("引数エラー");
+                }
+                else Console.WriteLine("引数エラー");
+            }
             else if (args[0] == "_tof")
             {
-                printmetftlist(x, y);
-                //for (int meta = 1; meta <= 10; meta++) {
-                //  double feet = metafeet(meta);
-                //   Console.WriteLine($"{meta}m={feet:o.oooo}ft");
-                //}
+                Console.WriteLine("表示範囲1(スタート)を決めてください");
+                string? inptnum = Console.ReadLine();
+                if (int.TryParse(inptnum, out var x))
+                {
+                    x = int.Parse(inptnum);
+
+                    Console.WriteLine("表示範囲2(ゴール)を決めてください");
+                    string? inputn = Console.ReadLine();
+                    if (int.TryParse(inputn, out var y))
+                    {
+                        y = int.Parse(inputn);
+
+                        printmetftlist(x, y);
+                        //for (int meta = 1; meta <= 10; meta++) {
+                        //  double feet = metafeet(meta);
+                        //   Console.WriteLine($"{meta}m={feet:o.oooo}ft");
+                        //}
+                    }
+                    else Console.WriteLine("引数エラー");
+                }
+                else Console.WriteLine("引数エラー");
             }
             else
                 Console.WriteLine("引数エラー");
---
+++ chapter04/文字列@p108/ko-do4_34/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ko_do4_34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "jjj";
+            if (int.TryParse(str, out var height))
+            {
+                Console.WriteLine("へんかん成功");
+            }
+            else
+                Console.WriteLine("変換できません");
+
+
+        }
+        try{int num = int.TryParse(str);
+    }catch(Excaption){
+    
+}  
