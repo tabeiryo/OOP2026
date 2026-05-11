2e2d6f9425749af5eb334ab484137590e2c6e868
田部井　涼 <oibc37091@ojs.ac.jp>
2026-05-11 11:16:14 +0900

    2no1no4

---
+++ chapter02/演習問題/exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace exercise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var songs = new Song[] {
+                new Song("letitbe", "THEbeetles",243),
+                new Song("BREigeoverTrionblidwalre", "simon&Gaffunkei", 293),
+                new Song("Gloss Toyo", "Gerpantites", 276),
+                new Song("Honstry", "Sliiy deal", 231),
+                new Song("I Will Alwersloveyou", "Wilitney hongston", 273),
+            };
+            PrintSongs(songs);
+        }
+
+        private static void PrintSongs(Song[] songs)
+        {
+            foreach (var n in songs)
+            {
+
+                Console.WriteLine(n.Title + "/" + n.ArtistName + "/" + n.Length / 60 + ":" + n.Length % 60);
+            }
+        }
+
+        //private static void PrintSongs(object songs)
+        //{
+        //    foreach (var n in songs)
+        //    {
+
+        //    }
+        //}
+    }
+}
---
+++ chapter02/演習問題/exercise01/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise01
+{
+    internal class Song
+    {
+        public string Title { get; set; } = string.Empty;
+        public string ArtistName { get; set; } = string.Empty;
+        public int Length { get; set; }
+
+        public Song(string title,string artistname,int length) {
+        Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
