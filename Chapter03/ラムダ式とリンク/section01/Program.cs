326c409a295f395b8611192d33aa61e918c9db29
田部井　涼 <oibc37091@ojs.ac.jp>
2026-05-20 10:34:45 +0900

    p71

--- Chapter03/ラムダ式とリンク/section01/Program.cs
+++ Chapter03/ラムダ式とリンク/section01/Program.cs
@@ -5,42 +5,29 @@ namespace section01
 {
     internal class Program
     {
-        public delegate bool Judgement(int value);
-
-        static int Count(int num) {
-            var numbers = new int[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            var count = 0;
-
-            Judgement judge = ISEEBN;
-
-            var count Count(numbers n => n%2==0);
-            Console.WriteLine(count);
-
-            static int Count(int[] numbers.Predicate<int> judge) { 
-            var count = 0;
-foreach (var n in numbers)
-            {
-                if (judge(in)==true)
-                {
-                    count++;
-                }
-            }
-            return count;
-        }
-            }
-
-
-
-            
         static void Main(string[] args)
         {
-            int count = 5;
-            Console.WriteLine(Count(count));
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-        }
+            var exists = cities.Exists(s => s[0] == 'A');
+            Console.WriteLine(exists);
 
-        static bool ISEEBN(int n) { 
-        return (n%2==0);
+            var name = cities.Find(s => s.Length == 6);
+            Console.WriteLine(name);
+
+            int index = cities.FindIndex(s => s =="Berlin");
+            Console.WriteLine(index);
+
+            
         }
 
     }
