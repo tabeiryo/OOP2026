366a0fdfcbcd0d345346fda414ebb5e84e59f3c5
田部井　涼 <oibc37091@ojs.ac.jp>
2026-05-21 10:06:09 +0900

    3.1おわり

---
+++ Chapter03/ラムダ式とリンク/section04/section04/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace section04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+            //IEnumerable<string> query = cities.Where(s => s.Length <= 5).Select(s => s.ToLower());
+            //foreach (string s in query) { Console.WriteLine(s); }
+
+            var query = cities.Where(s => s.Length <= 5).ToList();
+                foreach (var item in query) {
+                    Console.WriteLine(item);
+                }
+                
+            
+            Console.WriteLine("------------");
+
+            cities[0] = "Osaka";
+            foreach (var item in query) { Console.WriteLine(item); }
+            {
+
+            }
+        }
+    }
+}
---
+++ Chapter03/演習問題/exisis01/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace exisis01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers)
+        {var exists = numbers.Exists(s => s%8 ==0 || s%9 ==0);
+            if (exists == true)
+            {
+                Console.WriteLine("ある");
+            }else Console.WriteLine("ない");
+        }
+
+                private static void Exercise2(List<int> numbers)
+                {
+            numbers.ForEach(s => Console.WriteLine(s / 2));
+                }
+
+        private static void Exercise3(List<int> numbers)
+        {
+            IEnumerable<int> query = numbers.Where(s => s >= 50);
+            foreach (var i in query)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void Exercise4(List<int> numbers)
+        {
+            var query = numbers.Select(s => s * 2);
+            foreach (var i in query)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
