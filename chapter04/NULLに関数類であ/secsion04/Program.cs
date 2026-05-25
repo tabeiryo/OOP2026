561645712864a382cc964d9b2aaa199e66d294c0
田部井　涼 <oibc37091@ojs.ac.jp>
2026-05-25 10:48:18 +0900

    null,sale

---
+++ chapter04/NULLに関数類であ/secsion04/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace secsion04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? name = null;
+            if (name is null)
+                Console.WriteLine("nameはNULL");
+            string oode = "12345";
+
+
+
+            var messege = GetMessege(oode) ??  DefaultMessage();
+            Console.WriteLine(messege);
+
+
+
+            messege = null;
+            messege ??= DefaultMessage();
+
+
+
+            Sale sale = new Sale() {
+                Shopname = "aaa",
+            productCategory = "bbb",
+            Amount = 100
+            };
+
+            int? amount = sale ? Amount;
+            Console.WriteLine("売上高:" +amount);
+
+
+        }
+
+        private static string? DefaultMessage()
+        {
+            return "DefaultMessage" ;
+        }
+
+        private static string? GetMessege(string oode)
+        {
+            return null ;
+        }
+        public class Sale
+        {
+            public string Shopname { get; set; } = string.Empty;
+
+            public string productCategory { get; set; } = string.Empty;
+
+            public int Amount { get; set; }
+
+
+        }
+    }
+}
