74ac0816d5aae93392dd88e386c38e8940721a85
田部井　涼 <oibc37091@ojs.ac.jp>
2026-06-19 11:02:00 +0900

    7.1おわり。

---
+++ Chapter07/新しいフォルダー/section01/Program.cs
@@ -0,0 +1,37 @@
+﻿using Section01;
+using System.Threading.Channels;
+
+namespace section01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var numbers = new List<int> { 9, 7, 5, 4, 22, 5, 4, 0, 4, 1, 0, 4 };
+            var books = Books.GetBooks();
+            var priceAvelage = books.Average(x => x.Price);
+            var pageSUM = books.Sum(x => x.Pages);
+            var maxPrice = books.Max(x => x.Price);
+            var booook = books.FindAll(x => x.Price >= 500);
+            
+            
+
+
+            Console.WriteLine("平均金額:" + priceAvelage);
+            Console.WriteLine("合計ページ:" + pageSUM);
+            Console.WriteLine("高価な本:" + maxPrice);
+            foreach (var book in booook)
+            {
+                Console.WriteLine("500円以上の本:" + book.Title);
+            }
+            Console.WriteLine("250ページ以上の本上位3冊:");
+            var results = books.Where(n => n.Pages > 250).Take(3);
+            foreach (var item in results)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Exercise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = [5,10,17,9,3,21,10,40,21,3,35];
+
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers)
+        {
+            var maxnum = numbers.Max(x => x);
+            Console.WriteLine(maxnum);
+        }
+
+        private static void Exercise2(int[] numbers)
+        {
+            for (int i = numbers.Length - 2; i < numbers.Length; i++ )
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+        private static void Exercise3(int[] numbers)
+        {
+            
+           IEnumerable<string> query =numbers.Select(x => x.ToString());
+            foreach (var item in query)
+            {
+                if (item.Length == 2||item.Length==1)
+                {
+                    if (item.Length == 2)
+                    {
+                        string zero = "0";
+                        Console.WriteLine(zero +item);
+                    }
+                    else if (item.Length == 1) {
+                        string zero = "00";
+                        Console.WriteLine(zero + item);
+                    }
+
+                    
+                }
+                else Console.WriteLine(item);
+
+
+            }
+        }
+
+        private static void Exercise4(int[] numbers)
+        {
+            var s = 0;
+            var sortednum = numbers.OrderBy(x => x);
+            foreach (var item in sortednum) {
+                
+                if (s<3)
+                {
+                    Console.WriteLine(item);
+                }
+
+                s++;
+            }
+        }
+
+        private static void Exercise5(int[] numbers)
+        {
+            var results = numbers.Distinct();
+            var count = results.Count(f => f > 10);
+            Console.WriteLine(count);
+        }
+    }
+}
