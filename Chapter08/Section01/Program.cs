d4f4c14df269327973c1ab300a2898155fda46d5
田部井　涼 <oibc37091@ojs.ac.jp>
2026-06-25 15:19:15 +0900

    メニューまでできた

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Section01
+{
+    internal class Program
+    {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
+
+        static void Main(string[] args)
+        {
+            string pref, prefCeptallocation;
+            Console.WriteLine("県庁所在地の入力入力終了Ctrl+z");
+            Console.WriteLine();
+
+            while (true)
+            {
+                //都道府県
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+                if (pref == null) { break; }
+
+                //県庁所在地
+                Console.Write("県庁所在地:");
+                prefCeptallocation = Console.ReadLine();
+                if (prefCeptallocation == null) { break; }
+                Console.WriteLine();
+
+                //登録
+
+
+                prefOfficeDict.Add(pref, prefCeptallocation);
+            }
+
+            while (true)
+            {
+                //menu
+                Console.WriteLine("*****メニュー*****");
+                Console.WriteLine("1:一覧表示");
+                Console.WriteLine("2:検索");
+                Console.WriteLine("9:終了");
+                Console.WriteLine();
+
+               string menu=Console.ReadLine();
+
+                //終了
+                if (menu == "9") { break; }
+
+                //検索
+                else if (menu == "2")
+                {
+                    Console.Write("都道府県:");
+                    string find = Console.ReadLine();
+                    var key = find;
+                    if (prefOfficeDict.ContainsKey(key))
+                    {
+                        var fiin = prefOfficeDict[key];
+                        Console.WriteLine(key + "の県庁所在地は" + fiin);
+                        Console.WriteLine();
+                    }
+                }
+                //表示
+                else if (menu == "1")
+                {
+                    foreach (var f in prefOfficeDict) { Console.WriteLine("[都道府県:"+f.Key+"][県庁所在地:"+f.Value+"]"); }
+                }
+
+                else break;
+            }
+
+
+
+
+        }
+    }
+}
