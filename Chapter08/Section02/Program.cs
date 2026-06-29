7a299daf909fd36084865ec8c8b99ebef7c7e1b8
田部井　涼 <oibc37091@ojs.ac.jp>
2026-06-29 15:49:41 +0900

    Exicise01

---
+++ Chapter08/Section02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Section02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Sample.txt");
+            var we = new WordsExtractor(lines);
+            foreach (var line in we.Extract()) { Console.WriteLine(line); }
+        }
+    }
+
+    
+}
---
+++ Chapter08/演習問題/Exicise01/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Exicise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exicise01(text);
+            Console.WriteLine();
+            Exicise02(text);
+        }
+
+        private static void Exicise01(string text)
+        {
+            
+        }
+
+        private static void Exicise02(string text)
+        {
+            
+        }
+    }
+}
