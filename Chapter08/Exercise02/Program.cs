96ea67a9f79651e46518688e478382b6e0d08e73
田部井　涼 <oibc37091@ojs.ac.jp>
2026-07-02 14:01:15 +0900

    2

---
+++ Chapter08/Exercise02/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+
+            // Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核拡散防止条約");
+
+            // 8.2.3 (Countの呼び出し例)
+            // 上のAddメソッドで、２つのオブジェクトを追加しているので、読み込んだ単語数+2が、Countの値になる。
+
+            Console.WriteLine(abbrs.Count);
+
+
+
+
+            Console.WriteLine();    //改行
+
+            // 8.2.3 (Removeの呼び出し例)
+            if (abbrs.Remove("IOC"))
+            {
+                Console.WriteLine("成功");
+            }else Console.WriteLine("失敗");
+
+
+
+
+            // すでに削除してあるので、falseが返る
+            if (!abbrs.Remove("NPT"))
+            {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            // 8.2.4
+            // 新たなGetAllメソッドを追加済みなので、使用してLINQで処理を行う
+
+
+
+
+
+
+        }
+    }
+}
