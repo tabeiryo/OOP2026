3d9cd2df7d396796dff24cebbb942bab3c7562f2
田部井　涼 <oibc37091@ojs.ac.jp>
2026-08-25 15:48:29 +0900

    save

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -1,8 +1,12 @@
-﻿namespace CarReportSystem {
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace CarReportSystem {
     public sealed class Settings
     {
         //唯一のSettingsOBJ
        private static readonly Settings _instance = new Settings();
+        private Stream FileName;
 
         //メイン画面に設定した色情報
         public int MainFormBackColor { get; set; }
@@ -16,7 +20,19 @@
         private Settings() { }
 
 
+        public void  Save() { 
+        var date  = new SettingsDate{MainFormBackColor = MainFormBackColor };
 
+            using var writer = XmlWriter.Create(FileName);
+            var serializer = new XmlSerializer(typeof(SettingsDate));
+            serializer .Serialize(writer, date);
+        }
+
+
+        //保存用
+        public　class SettingsDate { 
+        public int MainFormBackColor { get; set; }
+        }
 
     }
 }
